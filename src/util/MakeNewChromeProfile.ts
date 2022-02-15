6a950c042d7e93cbb6acca16780db02a76954713
Daniel Reynolds <danielreynolds101@gmail.com>
2022-02-15 14:15:01 -0700

    tsify doneish

--- src/util/MakeNewChromeProfile.ts
+++ src/util/MakeNewChromeProfile.ts
@@ -5,7 +5,7 @@ const __dirname = dirname(__filename);
 import fs from 'fs';
 
 const make_new_chrome_profile = (profile_id: string) => {
-    const user_data_dir = `${__dirname}/temp/${profile_id}`
+    const user_data_dir = `/home/drg101/Personal/specter/temp/${profile_id}`
     console.log(`New user data dir ${user_data_dir}`)
     fs.mkdirSync(user_data_dir)
     return user_data_dir;
